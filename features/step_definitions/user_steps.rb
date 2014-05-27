e87de386bf0be45a8d3c9f88764dbc6262742e42
David G Chaves <davidgchaves@gmail.com>
2014-05-27 12:29:52 +0200

    Pass 'Given a User has posted the following messages...' step

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -7,6 +7,14 @@ Given /^the User has posted the message "([^"]*)"$/ do |message_text|
   FactoryGirl.create :message, content: message_text, user: User.first
 end
 
+Given /^a User has posted the following messages:$/ do |messages|
+  user = FactoryGirl.create :user
+  messages_attributes = messages.hashes.map do |message_attributes|
+    message_attributes.merge user: user
+  end
+  Message.create! messages_attributes
+end
+
 When /^I visit the page for the User$/ do
   expect(User.count).to eq 1
   visit user_path User.first
