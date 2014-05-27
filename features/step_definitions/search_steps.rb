a0a0cb375d74a7561c34d7c1d39693f5900e0328
David G Chaves <davidgchaves@gmail.com>
2014-05-27 14:19:47 +0200

    Pass 'When I enter "I am" in the search field' step

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -4,6 +4,11 @@ When /^I search for "([^"]*)"$/ do |query|
   click_button "Search"
 end
 
+When /^I enter "([^"]*)" in the search field$/ do |query|
+  visit "/search"
+  fill_in "query", with: query
+end
+
 Then /^the results are:$/ do |expected_results|
   results = [['content']] + page.all('ol.results li').map do |li|
     [li.text]
